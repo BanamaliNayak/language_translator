aa6619e94fda445b82415d35ded1713f38d7f694
BanamaliNayak <nayakbanamali00@gmail.com>
2024-06-22 19:38:17 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:language_translator/language_translation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Language Translator',
+      debugShowCheckedModeBanner: false,
+      home: LanguageTranslation(),
+    );
+  }
+}
